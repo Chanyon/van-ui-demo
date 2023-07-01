b6a566a0504c4b8c9a056f30cc340559147aa136
dcyun <Dominic_Duan@163.com>
2023-07-02 00:06:23 +0800

    Add leafer.js

--- src/views/profile/index.ts
+++ src/views/profile/index.ts
@@ -1,8 +1,35 @@
 import van from "../../../lib/van-0.12.2";
+import { Leafer, Rect, Text } from "leafer-ui";
 const { div } = van.tags;
 
 const Profile = () => {
-  return div({ class: "home" }, "profile");
+  const div_dom = div(); // div() => HTMLDivElement
+  const leafer = new Leafer({
+    view: div_dom,
+    width: 600,
+    height: 600,
+    hittable: true,
+  });
+  const rect = new Rect({
+    x: 100,
+    y: 100,
+    width: 200,
+    height: 200,
+    fill: '#32cd79',
+    stroke: 'black', 
+    strokeWidth: 2,
+    cornerRadius: 20,
+  });
+
+  const text = new Text({
+    fill: 'rgb(50,205,121)',
+    text: 'Hello LeaferJS',
+  });
+
+  leafer.add(rect);
+  leafer.add(text);
+
+  return div({ class: "profile" }, div_dom);
 };
 
 export {
